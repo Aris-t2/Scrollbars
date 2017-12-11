933c2e568c757095db611bd9063597847d86e224
Aris-t2 <aris-addons@gmx.net>
2017-12-11 09:04:40 +0100

    1.0.1fix

--- method 2/profile/userChrome/custom_scrollbars.uc.js
+++ method 2/profile/userChrome/custom_scrollbars.uc.js
@@ -7,7 +7,7 @@
 
 /* ******************************************************************************************** */
 /* Custom Scrollbars for Firefox ************************************************************** */
-/* version 1.0.1 ****************************************************************************** */
+/* version 1.0.0 ****************************************************************************** */
 /* ******************************************************************************************** */
 
 /* ***********************************************************************************************
@@ -26,44 +26,43 @@
  ENABLING options > set var to true
  DISABLING options > set var to false
  Modifying appearance > change values
- - color - name: red, blue, transparent / hex code: #33CCFF, #FFF
- - color - rgb(a): rgba(0,0,255,0.8) / hsl(a): hsla(240,100%,50%,0.8)
+ - color - name: blue / hex code: #33CCFF / rgb(a): rgba(0,0,255,0.8) / hsl(a): hsla(240,100%,50%,0.8)
  - numbers: 1, 2, 3 ... 10, 11, 12 ...
  - opacity: 0.0 to 1.0 e.g. 1.4, 1,75
  - gradients: linear-gradient(direction, color, color, color)
  - gradients example: linear-gradient(to right, blue, #33CCFF, rgba(0,0,255,0.8))
  
  NOTE
- - This is a tiny collection of scrollbar tweaks, not a port of 'NewScrollbars' add-on!
- - Small scrollbar width values will corrupt some parts of the ui!
+ - this is not a port of 'NewScrollbars' add-on, just a collection of scrollbar tweaks!
+ - small scrollbar width values will corrupt some parts of the ui!
  
 *********************************************************************************************** */
 
 // General scrollbar settings
-var hide_scrollbars = false; /* default = false */
-var hide_scrollbar_buttons = false; /* default = false */
-var custom_scrollbar_width = true; /* default = false */
-var custom_scrollbar_width_value = 17; /* 10-? // default = 17 (in px) */
-var custom_scrollbar_opacity = false; /* default = false */
-var custom_opacity_value = "1.0"; /* default = 1.0 */
-var enable_floating_scrollbars = false; /* scrollbars on top of web content */
+var hide_scrollbars = false; /* default > false */
+var hide_scrollbar_buttons = true; /* default > false */
+var custom_scrollbar_width = true; /* default > false */
+var custom_scrollbar_width_value = 7; /* 10-? // default > 17 (in px) */
+var custom_scrollbar_opacity = false; /* default > false */
+var custom_opacity_value = "1.0"; /* default > 1.0 */
+var enable_floating_scrollbars = true; /* scrollbars on top of web content */
 
 // Custom scrollbar appearance settings - "cs"
 var enable_custom_scrollbars = true;
-var cs_background_color = "#CCCCCC"; /* default = #CCCCCC */
-var cs_background_image = "linear-gradient(to right,transparent,rgba(255,255,255,0.5),transparent)"; /* default = none */
-var cs_thumb_color = "#33CCFF"; /* default = #33CCFF */
-var cs_thumb_image = "linear-gradient(to right,transparent,rgba(255,255,255,0.5),transparent)"; /* default = unset */
-var cs_thumb_hover_color = "#66FFFF"; /* default = #66FFFF */
-var cs_thumb_hover_image = "linear-gradient(to right,transparent,rgba(255,255,255,0.5),transparent)"; /* default = unset */
-var cs_thumb_roundness = 0; /* default = 0 (in px) */
-var cs_thumb_border = 1; /* default = 0 (in px) */
+var cs_background_color = "transparent"; /* default > #CCCCCC */
+var cs_background_image = "linear-gradient(to right,transparent,transparent,transparent)"; /* default > none */
+var cs_thumb_color = "#33CCFF"; /* default > #33CCFF */
+var cs_thumb_image = "linear-gradient(to right,transparent,rgba(255,255,255,0.3),transparent)"; /* default > none */
+var cs_thumb_hover_color = "#33CCFF"; /* default > #66FFFF */
+var cs_thumb_hover_image = "linear-gradient(to right,transparent,rgba(255,255,255,0.5),transparent)"; /* default > none */
+var cs_thumb_roundness = 20; /* default > 0 (in px) */
+var cs_thumb_border = 1; /* default > 0 (in px) */
 var cs_thumb_border_color = "#33CCFF"; /* default ##33CCFF */
-var cs_buttons_color = "#000000"; /* default = #000000 */
-var cs_buttons_image = "linear-gradient(to right,transparent,rgba(255,255,255,0.5),transparent)"; /* default = unset */
-var cs_buttons_hover_color = "#000066"; /* default = #000066 */
-var cs_buttons_hover_image = "linear-gradient(to right,transparent,rgba(255,255,255,0.5),transparent)"; /* default = unset */
-var cs_buttons_roundness = 0; /* default = 0 (in px) */
+var cs_buttons_color = "#000000"; /* default > #000000 */
+var cs_buttons_image = "linear-gradient(to right,transparent,transparent,transparent)"; /* default > none */
+var cs_buttons_hover_color = "#000066"; /* default > #000066 */
+var cs_buttons_hover_image = "linear-gradient(to right,transparent,transparent,transparent)"; /* default > none */
+var cs_buttons_roundness = 0; /* default > 0 (in px) */
 
 
 /* ******************************************************************************************** */
@@ -76,6 +75,7 @@ var cs_buttons_roundness = 0; /* default = 0 (in px) */
 
 Components.utils.import("resource://gre/modules/Services.jsm");
 var ss =  Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+
 var custom_scrollbars = {
 
   init: function() {
@@ -186,6 +186,10 @@ var floating_scrollbars = {
 	scrollbar {\
 	  position: relative !important;\
 	  z-index: 1000000000 !important;\
+	}\
+	scrollbar, scrollcorner {\
+	  background-color: transparent !important; \
+	  background-image: unset !important; \
 	}\
 	scrollbar[orient="vertical"] {\
 	  -moz-margin-start: -'+custom_scrollbar_width_value+'px !important;\
